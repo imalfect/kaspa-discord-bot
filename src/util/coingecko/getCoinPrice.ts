432b58ee8c88948d93025b687dfce78da477cfb7
iMalFect <me@imal.dev>
2025-01-14 19:21:28 +0100

    use ofetch instead of fetch

--- src/util/coingecko/getCoinPrice.ts
+++ src/util/coingecko/getCoinPrice.ts
@@ -1,8 +1,14 @@
-export default async function getCoinPrice(coinId: string, currency: string): Promise<number> {
-	return await fetch(
-		`https://api.coingecko.com/api/v3/simple/price?ids=${coinId}&vs_currencies=${currency}`
-	)
-		.then((response) => response.json())
+import { ofetch } from 'ofetch';
+
+export default async function getCoinPrice(
+	coinId: string,
+	currency: string
+): Promise<number | null> {
+	return await ofetch<{
+		[coinId: string]: {
+			[currency: string]: number;
+		};
+	}>(`https://api.coingecko.com/api/v3/simple/price?ids=${coinId}&vs_currencies=${currency}`)
 		.then((data) => data[coinId][currency])
 		.catch(() => null);
 }
