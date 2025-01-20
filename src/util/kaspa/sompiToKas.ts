8eae5148a107ce2c222ae34c35d78b6515786108
iMalFect <me@imal.dev>
2025-01-20 23:19:25 +0100

    add number to param types of sompiToKas

--- src/util/kaspa/sompiToKas.ts
+++ src/util/kaspa/sompiToKas.ts
@@ -1,6 +1,6 @@
 import Big from 'big.js';
 
-export default function sompiToKas(sompi: bigint | string): number {
+export default function sompiToKas(sompi: bigint | string | number): number {
 	// @ts-expect-error	The typing is wrong, Big takes bigint
 	return Big(sompi).div(1e8).toNumber();
 }
