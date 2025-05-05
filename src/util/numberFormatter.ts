e2207da0ec6d2163bd075c64dd71462739cb7fd1
iMalFect <me@imal.dev>
2025-05-05 17:49:21 +0200

    fix incorrect display temporarily

--- src/util/numberFormatter.ts
+++ src/util/numberFormatter.ts
@@ -1,5 +1,5 @@
 export function formatNumber(number: number | string, decimalPoints = 2) {
-	return number.toLocaleString('en-US', {
+	return parseFloat(number as string).toLocaleString('en-US', {
 		style: 'decimal',
 		useGrouping: true,
 		minimumFractionDigits: decimalPoints,
