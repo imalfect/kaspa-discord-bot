cb148cd352ef14979a51db1a397c8d0fd400db0c
iMalFect <me@imal.dev>
2025-05-23 16:57:25 +0200

    remove crescendo countdown feature

--- src/kaspa/function/secondsUntilCrescendo.ts
+++
@@ -1,8 +0,0 @@
-import { kaspa } from '@/kaspa';
-
-export default async function secondsUntilCrescendo() {
-	const dagInfo = await kaspa.getBlockDagInfo();
-	const daaScore = parseInt(dagInfo.virtualDaaScore.toString());
-	const goal = 110165000;
-	return goal - daaScore;
-}
