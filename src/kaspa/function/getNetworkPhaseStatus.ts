719a1410cd31291dc760aea6fbb7c3aba976e6c4
iMalFect <me@imal.dev>
2025-06-26 12:52:24 +0200

    fix next phase calculation

--- src/kaspa/function/getNetworkPhaseStatus.ts
+++ src/kaspa/function/getNetworkPhaseStatus.ts
@@ -1,17 +1,36 @@
-import { DEFLATIONARY_TABLE } from '@/kaspa/constants.ts';
+import { CRESCENDO_ACTIVATION_DAA, DEFLATIONARY_TABLE } from '@/kaspa/constants.ts';
 import type { BlockRewardReductionRecord } from '@/kaspa/types.ts';
 import Big from 'big.js';
-import dayjs from 'dayjs';
 
 export default function getNetworkPhaseStatus(daaScore: bigint) {
 	const nextPhaseIndex = DEFLATIONARY_TABLE.findIndex((phase) => phase.daaStart > daaScore);
 	const nextPhase = DEFLATIONARY_TABLE[nextPhaseIndex];
 	const currentPhase = DEFLATIONARY_TABLE[nextPhaseIndex < 1 ? 0 : nextPhaseIndex - 1];
-	// @ts-expect-error The typing is wrong, Big takes bigint
-	const nextPhaseStart = dayjs().add(Big(nextPhase.daaStart).minus(daaScore).toNumber(), 'seconds');
+	let nextPhaseStart;
+	if (nextPhase) {
+		const bigDaaScore = new Big(daaScore.toString());
+		const bigNextPhaseDaaStart = new Big(nextPhase.daaStart);
+		const bigCrescendoActivationDaa = new Big(CRESCENDO_ACTIVATION_DAA);
+
+		const daaScoreRemaining = bigNextPhaseDaaStart.minus(bigDaaScore);
+		const secondsRemaining = daaScoreRemaining.gt(0)
+			? bigDaaScore.lt(bigCrescendoActivationDaa) && bigNextPhaseDaaStart.gt(bigCrescendoActivationDaa)
+				? bigCrescendoActivationDaa
+						.minus(bigDaaScore)
+						.plus(bigNextPhaseDaaStart.minus(bigCrescendoActivationDaa).div(10))
+						.toNumber()
+				: (bigDaaScore.lt(bigCrescendoActivationDaa)
+						? daaScoreRemaining
+						: daaScoreRemaining.div(10)
+					).toNumber()
+			: 0;
+
+		nextPhaseStart =
+			secondsRemaining > 0 ? new Date(Date.now() + secondsRemaining * 1000) : undefined;
+	}
 	return {
 		currentPhase,
 		nextPhase: nextPhase as BlockRewardReductionRecord | undefined,
-		nextPhaseStart: nextPhaseStart ? nextPhaseStart.format('YYYY-MM-DD') : undefined
+		nextPhaseStart: nextPhaseStart ? nextPhaseStart.toISOString().slice(0, 10) : undefined
 	};
 }
