4f082784d2d90a644a52dc199cb6e4e74b17b074
iMalFect <me@imal.dev>
2025-09-21 20:00:41 +0200

    register custom protocol outside of function

--- src/util/sanitizeMessage.ts
+++ src/util/sanitizeMessage.ts
@@ -1,17 +1,18 @@
 // Removing gifs, links and mentions from messages
 import * as linkify from 'linkifyjs';
 import removeMd from 'remove-markdown';
+
+linkify.registerCustomProtocol('discord', true)
+
 export default function sanitizeMessage(message: string) {
 	// Remove links (should also effectively remove gifs)
 	message = removeMd(message);
-	linkify.registerCustomProtocol('discord', true)
 	const links = linkify.find(message);
 	if (links.length) {
 		for (const link of links) {
 			message = message.replace(link.value, '');
 		}
 	}
-
 	// Remove mentions
 	message = message.replace(/<@!?\d+>/g, '');
 	return message;
