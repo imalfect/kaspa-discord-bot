0222a99fcd5207143d030b1f6bc3b054e0c152ca
iMalFect <me@imal.dev>
2025-09-21 19:57:58 +0200

    register discord custom protocol (geniuses from scam servers try to trick linkify)

--- src/util/sanitizeMessage.ts
+++ src/util/sanitizeMessage.ts
@@ -4,12 +4,14 @@ import removeMd from 'remove-markdown';
 export default function sanitizeMessage(message: string) {
 	// Remove links (should also effectively remove gifs)
 	message = removeMd(message);
+	linkify.registerCustomProtocol('discord', true)
 	const links = linkify.find(message);
 	if (links.length) {
 		for (const link of links) {
 			message = message.replace(link.value, '');
 		}
 	}
+
 	// Remove mentions
 	message = message.replace(/<@!?\d+>/g, '');
 	return message;
